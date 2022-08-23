420e75a80f2985209cbb70c77540e127717414f9
Jerome <jerome.siljan@mavs.uta.edu>
2022-08-23 18:24:30 -0500

    Confirming embeddings

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+# Imports
 import pandas as pd
 import numpy as np
-
 from sklearn.model_selection import train_test_split
-
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
@@ -15,11 +14,27 @@ data = df.values.tolist()
 x_vals = [entry[1] for entry in data]
 y_vals = [entry[0] for entry in data]
 
-x_train, x_test, y_train, y_test = train_test_split(x_vals, y_vals, stratify=y_vals, random_state=0)
+vocab_max = 100000
+tokenizer = Tokenizer(num_words=vocab_max, oov_token='<OOV>')
 
-#print(x_train)
+tokenizer.fit_on_texts(x_vals)
+x_index = tokenizer.word_index
 
-tokenizer = Tokenizer(oov_token='<OOV>')
-tokenizer.fit_on_texts(x_train)
-#sequences_x_train = 
-print(x_train)
+seq_x_index = tokenizer.texts_to_sequences(x_vals)
+headlines = pad_sequences(seq_x_index, padding = 'post')
+
+print(
+
+"""
+headline_train, headline_test, category_train, category_test = train_test_split(headlines, categories, random_state=0, stratify=categories)
+
+print(f'{headline_train[0]}, {category_train[0]}')
+
+target_headline = headlines[0]
+
+for word in target_headline: 
+    if word != 0:
+        for key, value in x_index.items():
+            if value == word:
+                print(f'{value}, {key}')
+"""
