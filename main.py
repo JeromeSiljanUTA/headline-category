a938da64f8ff321e867cc7ac84756fdb20ca63eb
Jerome <jerome.siljan@mavs.uta.edu>
2022-09-05 10:11:21 -0500

    got sequences working

--- main.py
+++ main.py
@@ -5,6 +5,18 @@ from sklearn.model_selection import train_test_split
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
+def decode_headline(seq):
+    for thing in seq:
+        for key, value in x_index.items():
+            if value == thing:
+                print(f'{value}, {key}')
+
+def decode_category(seq):
+    for thing in seq:
+        for key, value in y_index.items():
+            if value == thing:
+                print(f'{value}, {key}')
+
 df = pd.read_json('news_dataset.json', lines=True)
 
 df.drop(columns=['link', 'authors', 'short_description', 'date'], inplace=True)
@@ -19,18 +31,19 @@ tokenizer = Tokenizer(num_words=vocab_max, oov_token='<OOV>')
 
 tokenizer.fit_on_texts(x_vals)
 x_index = tokenizer.word_index
-
 seq_x_index = tokenizer.texts_to_sequences(x_vals)
 headlines = pad_sequences(seq_x_index, padding = 'post')
+
+tokenizer.fit_on_texts(y_vals)
+y_index = tokenizer.word_index
+seq_y_index = tokenizer.texts_to_sequences(y_vals)
+categories = pad_sequences(seq_y_index, padding = 'post')
 
 #print(x_index)
 print(x_vals[0])
 print(seq_x_index[0])
-arr = [379, 361, 155, 1086, 2319, 6, 397, 241, 78, 85, 267, 189, 10, 304]
+#arr = [379, 361, 155, 1086, 2319, 6, 397, 241, 78, 85, 267, 189, 10, 304]
 print(headlines[0])
-for thing in headlines[0]:
-    for key, value in x_index.items():
-        if value == thing:
-            print(f'{value}, {key}')
-
-
+decode_headline(headlines[0])
+print(categories[0])
+decode_category(categories[0])
